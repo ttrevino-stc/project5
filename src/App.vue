90f0f075e454465c455243a4bd89b1762dfbe755
Thomas Trevino <ttrevino@stu.southtexascollege.edu>
2025-03-02 18:56:23 -0600

    Implemented global timer state

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,46 @@
-<script setup>
-  import NavMenu from '@/components/NavMenu.vue'
+<script>
+import { ref, provide } from 'vue';
+import NavMenu from '@/components/NavMenu.vue'
+
+export default {
+  components: { NavMenu },
+  setup() {
+    const timeRemaining = ref(300);
+    let timerInterval = null;
+
+    const startTimer = () => {
+      if (!timerInterval) {
+        timerInterval = setInterval(() => {
+          if (timeRemaining.value > 0) {
+            timeRemaining.value--;
+          } else {
+            clearInterval(timerInterval);
+            timerInterval = null;
+          }
+        }, 1000);
+      }
+    };
+
+    const stopTimer = () => {
+      clearInterval(timerInterval);
+      timerInterval = null;
+    };
+
+    const resetTimer = () => {
+      stopTimer();
+      timeRemaining.value = 300;
+    };
+
+    provide('timer', { timeRemaining, startTimer, stopTimer, resetTimer });
+
+    return {};
+  }
+};
 </script>
 
 <template>
-  <NavMenu />
-  <RouterView />
+  <div class="min-h-screen bg-gray-100">
+    <NavMenu />
+    <RouterView />
+  </div>
 </template>
